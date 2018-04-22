dde728d08e362ce3233c95df9188a0e187d5672d
YY <273176329@qq.com>
2018-04-22 21:26:48 +0800

    Add files via upload

---
+++ server.js
@@ -0,0 +1,19 @@
+﻿//导入express框架
+var express = require('express');
+
+//初始化express
+var app = express();
+
+//express服务器监听
+app.listen(8000,'localhost',function(){
+
+   console.log('express is listening');
+
+});
+
+//路由设置
+app.get('/',function(req,res){
+
+  res.send('<h1>hello express</h1>');
+
+})
